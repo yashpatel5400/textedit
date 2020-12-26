d89a2d2bc14ba41ef362c4d2de79b24553389c6d
yashpatel5400 <ypatel@princeton.edu>
2020-12-26 12:15:34 -0500

    Added some more stuff

--- kilo.c
+++ kilo.c
@@ -68,8 +68,19 @@ char editorReadKey() {
 
 /*** output ***/
 
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 4);
+  }
+}
+
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+  
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
