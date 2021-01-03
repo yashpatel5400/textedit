5944a61e98c962fade04f21ddd84536990e5ba69
yashpatel5400 <ypatel5400@gmail.com>
2021-01-02 23:16:16 -0500

    Added text editing./kilo kilo.c

--- kilo.c
+++ kilo.c
@@ -25,6 +25,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+  BACKSPACE = 127,
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
@@ -458,6 +459,10 @@ void editorProcessKeypress() {
   int c = editorReadKey();
 
   switch (c) {
+    case 'r':
+      /* TODO */
+      break;
+
     case CTRL_KEY('q'):
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
@@ -471,6 +476,11 @@ void editorProcessKeypress() {
     case END_KEY:
       if (E.cy < E.numrows)
         E.cx = E.row[E.cy].size;
+      break;
+
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
       break;
 
     case PAGE_UP:
@@ -494,6 +504,10 @@ void editorProcessKeypress() {
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(c);
+      break;
+
+    case CTRL_KEY('l'):
+    case '\x1b':
       break;
 
     default:
